4527a0bd97296ce65148891350469326838f92db
Franci2008 <francibuff10@gmail.com>
2022-10-27 15:11:30 +0200

    [ADD] Creato il mio Primo Programma

---
+++ 2_lato_quadrato.py
@@ -0,0 +1,8 @@
+lato = 0
+perimetro_quadrato = 0
+area_quadrato = 0
+lato = int(input('Inserisci il Lato del Quadrato:'))
+area_quadrato = lato * lato
+perimetro_quadrato = lato * 4
+print ('Area del Quadtraro é:' , area_quadrato)
+print ('Perimetro del Quadtraro é:' , perimetro_quadrato)
