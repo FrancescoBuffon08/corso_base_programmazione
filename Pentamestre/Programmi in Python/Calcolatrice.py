1362774606c20ae6ee3843e92d9cac7bfa0b8f50
Franci2008 <francibuff10@gmail.com>
2023-02-22 15:42:37 +0100

    [ADD]Creato il mio primo programma

--- Pentamestre/Programmi in Python/Calcolatrice.py
+++ Pentamestre/Programmi in Python/Calcolatrice.py
@@ -5,10 +5,10 @@ def differenza(x, y):
 	risultato = numero_1 - numero_2
 	return risultato
 def moltiplicazione(x, y):
-	risultato = numero_1 + numero_2
+	risultato = numero_1 * numero_2
 	return risultato
 def divisione(x, y):
-	risultato = numero_1 + numero_2
+	risultato = numero_1 / numero_2
 	return risultato
 operazione:str
 numero_1=0
@@ -20,11 +20,18 @@ while True:
     numero_1=int(input('Inserisci il primo numero: '))
     numero_2=int(input('Inserisci il secondo numero che sia maggiore di 0: '))
     y_n=str(input('Vuoi continuare? Yes or No: '))
-    if(numero_2>0):
-        if(y_n=='yes'):
-            print(operazione(numero_1, numero_2))
-        else:
-            print('fine')
+    if(y_n=='yes'):
+        if(operazione=='somma'):
+            print(somma(numero_1, numero_2))
+        if(operazione=='differenza'):
+            print(differenza(numero_1, numero_2))
+        if(operazione=='moltiplicazione'):
+            print(moltiplicazione(numero_1, numero_2))
+        if(operazione=='divisione'):
+            if(numero_2>0):
+                print(divisione(numero_1, numero_2))
+            else:
+                print('non puoi dividere per 0!')
     else:
         print('fine')
     vuoi_fare_altri_conti=str(input('Vuoi fare altri calcoli? Yes or No: '))
